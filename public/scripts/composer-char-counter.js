a2ee6902becfd8b393f8a22a5ba7fb50e55ed276
Kevin Mason <krmason2020@gmail.com>
2021-11-17 06:30:28 +0000

    styling changes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,15 @@
+//Name:   Character count Javascript file
+//Written By: Rohit Dhand
+// Written on: 15th Nov, 2021
+// Purpose: This script actualy.....
+// Change Log:
+// 1. Changed by : Alex Date: 19th Nov.2021
+//What was changed:
+
+//Will ensure that the document loads before JQuery functions are called.
 $(document).ready(function () {
   // --- our code goes here ---
+  //On the key change event where we are capturing the total number of characters left and changing the color to red if they go less than 0
   $("#tweet-text").keydown(function () {
     let charCount = this.value.length;
     const counter = $(this).parentsUntil(".new-tweet").find(".counter");
@@ -12,4 +22,5 @@ $(document).ready(function () {
       counter.removeClass("over-limit");
     }
   });
+  //On the key change event function finishes.
 });
